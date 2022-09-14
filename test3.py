d97b56aff10b225b91fcec4d9c7df8552d1c6a7d
Imran <kazimran>
2022-09-14 16:51:53 +0600

    bs instant method summation task

---
+++ test3.py
@@ -0,0 +1,15 @@
+class addition1:  # class
+    def set_value(self,val1,val2):
+        self.val1 = val1
+        self.val2 = val2
+
+    def display(self):
+       print(self.val1+self.val2)
+
+
+
+
+
+rahim = addition1()  # object declare
+rahim.set_value(101,3.50)
+rahim.display()
